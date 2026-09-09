6fdcbf3bf7af06055ee09bcbf0690acf7330bf00
dabeycorn <Dasun20202020@gmail.com>
2026-09-09 02:32:34 -0400

    attempt to setup basic flake
    Signed-off-by: dabeycorn <Dasun20202020@gmail.com>

--- flake.nix
+++ flake.nix
@@ -1,5 +1,38 @@
 {
 	description = "Hiru's system configuration";
-	inputs = {};
-	outputs = {};
+	inputs = {
+		master.url = "github:nixos/nixpkgs/master";
+		stable.url = "github:nixos/nixpkgs/nixos-26.05";
+		unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+		home-manager.url = "github:nix-community/home-manager";
+		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+	};
+	outputs = { self, nixpkgs, ... } @inputs:
+		let
+			inherit (self) outputs;
+			forSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
+			system = "x86_64-linux";
+			pkgs = import nixpkgs {
+				inherit system;
+			};
+		in
+		{
+			nixosConfigurations = {
+				# TODO home manager
+				optimus = nixpkgs.lib.nixosSystem
+					{
+						specialArgs = {
+							inherit inputs outputs;
+						};
+						modules = [
+							"./hosts/optimus/configuration.nix"
+						];
+					};
+
+				# TODO setup metroplex
+				#metroplex = nixpkgs.lib.nixosSystem
+				#	{
+				#	};
+			};
+		};
 }
